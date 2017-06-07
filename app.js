6b7d6a24ec52dbb36e50092e4143610c3dfcaddd
gustavofp <gu.fpoletto@gmail.com>
2017-06-07 02:07:45 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('myApp', [
+  'ngRoute',
+  'myApp.home',
+  'myApp.service',
+  'myApp.version',
+  'slick'
+]).
+config(['$locationProvider', '$routeProvider', '$provide', function($locationProvider, $routeProvider, $provide) {
+
+  // FORMATA CURRENCY COM VIRGULA
+  $provide.decorator('$locale', ['$delegate', function ($delegate) {
+        $delegate.NUMBER_FORMATS.DECIMAL_SEP = ',';
+        return $delegate;
+    }]);
+
+
+  $locationProvider.hashPrefix('!');
+
+  $routeProvider.otherwise({redirectTo: '/home'});
+}]);
